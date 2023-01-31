e6b540057e6b8221297fc86b061bd865453d0e74
Bruno Lessa <brunolessa@live.com>
2023-01-31 13:54:31 -0300

    validations and errors message

--- src/server/controllers/city/Create.ts
+++ src/server/controllers/city/Create.ts
@@ -1,11 +1,30 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 
 type City = {
   name: string;
 };
 
-export const create = (req: Request<{}, {}, City>, res: Response) => {
-  console.log(req.body.name);
-  return res.status(StatusCodes.CREATED).send("created");
+const bodyValidation: yup.SchemaOf<City> = yup.object().shape({
+  name: yup.string().required().min(3),
+});
+
+export const create = async (req: Request<{}, {}, City>, res: Response) => {
+  try {
+    const validateData = await bodyValidation.validate(req.body, {
+      abortEarly: false,
+    });
+    console.log(validateData);
+    return res.status(StatusCodes.CREATED).send("created");
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+    const errors: Record<string, string> = {};
+
+    yupError.inner.forEach((e) => {
+      if (e.path) errors[e.path] = e.message;
+    });
+
+    return res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({ errors });
+  }
 };
